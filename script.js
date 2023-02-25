ff043e8a292b38c160cd31af7960aed2c5d7b3ae
joiellantero <jllantero@gmail.com>
2023-02-26 00:23:28 +0800

    update frontend

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-let form = document.getElementById('fqr')
-form.addEventListener('submit', e => {
+let fqr = document.getElementById('fqr')
+let fdowload = document.getElementById('fdownload')
+
+fqr.addEventListener('submit', e => {
   e.preventDefault()
 
   let url = e.target.elements.furl.value
-  let size = e.target.elements.fsize.value
 
-  console.log(url, size)
+  console.log(url)
 
-  new QRCode(document.getElementById("qrcode"), {
+  let wrapper = document.querySelector(".wrapper")
+  wrapper.classList.add("active");
+
+  new QRCode(document.querySelector("#qrimg"), {
     text: url,
-    width: size,
-    height: size,
+    width: 125,
+    height: 125,
     colorDark : "#000000",
     colorLight : "#ffffff",
     correctLevel : QRCode.CorrectLevel.H
   })
+})
+
+fdownload.addEventListener('submit', e => {
+  e.preventDefault()
+
+  let size = e.target.elements.fsize.value
+
+  console.log("download at size:", size, "x", size)
 })
