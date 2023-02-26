1978d6a6e0675b45fcf55500e62fc9112d63e2a3
joiellantero <jllantero@gmail.com>
2023-02-26 12:07:35 +0800

    add download feature

--- script.js
+++ script.js
@@ -1,30 +1,55 @@
-let fqr = document.getElementById('fqr')
-let fdowload = document.getElementById('fdownload')
+let fqr = document.getElementById('fqr');
+let fdowload = document.getElementById('fdownload');
+let qr = document.getElementById('qr')
 
 fqr.addEventListener('submit', e => {
-  e.preventDefault()
+  e.preventDefault();
 
-  let url = e.target.elements.furl.value
+  clearUI();
 
-  console.log(url)
+  let url = e.target.elements.furl.value;
+  let size = e.target.elements.fsize.value;
 
-  let wrapper = document.querySelector(".wrapper")
+  let wrapper = document.querySelector(".wrapper");
   wrapper.classList.add("active");
 
-  new QRCode(document.querySelector("#qrimg"), {
+  const qrimg = document.createElement('div');
+  qrimg.id = "qr-img";
+  document.getElementById('qr').appendChild(qrimg);
+
+  generateQR(url)
+  setTimeout(() => {
+    const url = document.querySelector("#qr-img img").src
+    createDownloadBtn(url, size);
+  }, 50);
+});
+
+const generateQR = (url) => {
+  new QRCode(document.getElementById("qr-img"), {
     text: url,
     width: 125,
     height: 125,
     colorDark : "#000000",
     colorLight : "#ffffff",
     correctLevel : QRCode.CorrectLevel.H
-  })
-})
+  });
+};
 
-fdownload.addEventListener('submit', e => {
-  e.preventDefault()
+const createDownloadBtn = (url, size) => {
+  const link = document.createElement('a');
+  link.id = 'save-img';
+  link.classList = 'btn btn-primary';
+  link.href = url
+  link.download = 'image.png';
+  link.innerHTML = 'Save Image';
 
-  let size = e.target.elements.fsize.value
+  document.getElementById('qr').appendChild(link);
+};
 
-  console.log("download at size:", size, "x", size)
-})
+const clearUI = () => {
+  qr.innerHTML = '';
+  const saveBtn = document.getElementById('save-link');
+  if (saveBtn) {
+    saveBtn.remove();
+  }
+};
